80ed10e37f8df20b68f997e67b490401f3b97c5d
AlpErdinc <97795127+AlpErdinc@users.noreply.github.com>
2023-02-04 15:05:21 -0500

    added rooting velocity stop

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     private Rigidbody2D playerRB;
+    private bool rooted;
    
     // Start is called before the first frame update
     private void Start()
@@ -15,10 +16,24 @@ public class playerMovement : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        float moveX = Input.GetAxisRaw("Horizontal");
-        playerRB.velocity = new Vector2(moveX*7f,0);
+        if(Input.GetKeyDown("space")){
+            rooted = true;
+            playerRB.velocity = new Vector2(0, 0);
+        }
+        if (!rooted)
+        {
+            float moveX = Input.GetAxisRaw("Horizontal");
+            playerRB.velocity = new Vector2(moveX * 7f, 0);
+        }
 
-        //if(Input.GetButtonDown("Horizontal"))
-                //playerRB.velocity = new Vector2 (14f, 0);
+
+        if (Input.GetKeyUp("space"))
+        {
+            rooted = false;
+        }
+        //if (Input.GetButtonDown("Space"))
+        //{
+
+        //}
     }
 }
