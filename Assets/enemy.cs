7300a77ec33a6da7fd36cce093acb3c165d8a7bc
BraveGuys42 <minhas.zaid@gmail.com>
2023-02-04 16:58:53 -0500

    Enemy Script

---
+++ Assets/enemy.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    Vector3 startPos;
+    public float distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
