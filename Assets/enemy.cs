45a2c6e064a83530041b953b90e8ed811cb19e6a
BraveGuys42 <minhas.zaid@gmail.com>
2023-02-04 19:21:11 -0500

    Enemy

--- Assets/enemy.cs
+++ Assets/enemy.cs
@@ -4,17 +4,62 @@ using UnityEngine;
 
 public class enemy : MonoBehaviour
 {
-    Vector3 startPos;
+    float startPos;
     public float distance;
+    public float idleTime = 3;
+    public float speed;
+    float waitingTime = -1;
+    bool facingRight = true;
+    bool idle = false;
+    float angle = 0;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        startPos = transform.position; 
+        startPos = transform.position.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //If Im not in idle time
+        if (Time.time > waitingTime)
+        {
+            if (transform.position.x <= startPos + distance && transform.position.x >= startPos)
+            {
+                if (idle)
+                {
+                    print("MOVE");
+                    //Back up
+                    if (facingRight)
+                    {
+                        angle = 0;
+                    }
+                    else
+                    {
+                        angle = 180;
+                    }
+                    transform.rotation = Quaternion.Euler(0f, angle, 0);  
+                    idle = false;
+                }
+                transform.Translate(Vector2.right * speed * Time.deltaTime);
+            }
+            else if (idle == false)
+            {
+                idle = true;
+                facingRight = !facingRight;
+                waitingTime = Time.time + idleTime;
+                transform.Translate(Vector2.left);
+                print("WHY");
+            }
+        }
+        else
+        {
+            transform.Rotate(0f, 180/idleTime * Time.deltaTime, 0f);
+            
+        }
+
     }
 }
