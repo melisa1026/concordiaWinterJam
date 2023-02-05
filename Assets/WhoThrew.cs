6d9f76fe50ee9c58e46de37efb83c6cf78635a9d
BraveGuys42 <minhas.zaid@gmail.com>
2023-02-05 11:29:13 -0500

    Who threw

---
+++ Assets/WhoThrew.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhoThrew : MonoBehaviour
+{
+    //0 for lumberjack
+    //1 for clown car
+    //2 for sleeper
+    public static int thrower = 0;
+}
